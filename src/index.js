0ad4c1ae8ddcc199fa2d09efe8318f3895cf3042
jenzie68 <jacecabalo@gmail.com>
2023-08-16 15:49:27 +0400

    install webpack, addProject function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log(`it works, awesome`);
+const allProject = [];
+
+const addProject = (title) => {
+    const allTasks = [];
+
+    const addTask = (taskName,detail = 'none') => {
+        allTasks.push({name: taskName, description: detail})
+        console.log(allTasks);
+    };
+
+    return {title, addTask, allTasks};
+}
+
+console.log(allProject);
+
+
+
