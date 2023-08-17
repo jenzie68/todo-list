547ef3e00652890f22b43940eb4704464dd0e2e9
jenzie68 <jacecabalo@gmail.com>
2023-08-17 19:34:40 +0400

    add delete feat in addproject func

--- src/index.js
+++ src/index.js
@@ -8,7 +8,30 @@ const addProject = (title) => {
         console.log(allTasks);
     };
 
-    return {title, addTask, allTasks};
+    const deleteTask = (index) => {
+        allTasks.splice(index,1);
+        console.log('task has been deleted');
+        console.log(allTasks);
+    }
+
+    const deleteProject = () => {
+        const alert = prompt('are you sure you want your project deleted?')
+        if(alert == 'yes') {
+        allProject.splice(allProject.indexOf(title),1);
+        }
+        console.log(allProject);
+    }
+
+    allProject.push(title);
+    console.log(allProject);
+
+    return {
+        title,
+        addTask, 
+        allTasks,
+        deleteProject,
+        deleteTask
+    };
 }
 
 console.log(allProject);
