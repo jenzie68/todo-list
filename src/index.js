2eb85ecdc8284a1da31159856fdb14197c5277d7
jenzie68 <jacecabalo@gmail.com>
2023-08-21 21:21:41 +0400

    add UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const allProject = [];
 
 const addProject = (title) => {
+    
     const allTasks = [];
 
     const addTask = (taskName,detail = 'none',dueDate, priorityList = 'not important') => {
@@ -19,7 +20,7 @@ const addProject = (title) => {
         allTasks.splice(index,1);
         console.log('task has been deleted');
         console.log(allTasks);
-    }
+    };
 
     const deleteProject = () => {
         const alert = prompt('are you sure you want your project deleted?')
@@ -29,7 +30,6 @@ const addProject = (title) => {
         console.log(allProject);
     }
 
-    allProject.push(title);
     console.log(allProject);
 
     return {
@@ -39,7 +39,41 @@ const addProject = (title) => {
         deleteProject,
         deleteTask
     };
-}
+};
+
+function makeBtn(name) {
+    const btn = document.createElement('button');
+    const projects = document.querySelector('.content');
+    btn.textContent = name;
+    projects.appendChild(btn);
+    btn.addEventListener('click', () => {
+        const addTaskbtn = document.createElement('button');
+        const tasks = document.querySelector('.tasks');
+        addTaskbtn.textContent = 'addtask';
+        tasks.appendChild(addTaskbtn);
+        addTaskbtn.addEventListener('click', () => {
+            const index = allProject.findIndex(i => i.title == name);
+            const taskName = prompt('task name');
+            allProject[index].addTask(taskName);
+            const div = document.createElement('div');
+            div.textContent = taskName;
+            const tasks = document.querySelector('.tasks');
+            tasks.appendChild(div);
+            console.log(allProject);
+        });
+    });
+};
+
+const btn = document.createElement('button');
+const content = document.querySelector('.content');
+btn.textContent = 'Add Project';
+content.appendChild(btn);
+btn.addEventListener('click',() => {
+    const title = prompt('your project name','study');
+    const project = addProject(title);
+    allProject.push(project);
+    makeBtn(title);
+});
 
 console.log(allProject);
 
